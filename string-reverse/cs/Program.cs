a3f69ad3fc03eab6c2624a0cc80b449a61c32202
Evgeniy A. Simonenko <easimonenko@mail.ru>
2016-12-03 21:56:46 +0300

    String reverse sample in C#.

---
+++ string-reverse/cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cs
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+
+            var char_array = s.ToCharArray();
+            Array.Reverse(char_array);
+
+            var r = new string(char_array);
+
+            Console.WriteLine(r);
+        }
+    }
+}
---
+++ string-reverse/v2-cs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace v2cs
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+
+            var builder = new StringBuilder(s);
+
+            for (int i = 0; i < builder.Length / 2; i++) {
+                var c = builder[i];
+                builder[i] = builder[builder.Length - 1 - i];
+                builder[builder.Length - 1 - i] = c;
+            }
+
+            Console.WriteLine(builder);
+        }
+    }
+}
